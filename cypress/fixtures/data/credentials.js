f9923556deba991b9bbe84eac10f0c83d3284eea
Markos9932 <vojinovicmarkoo@gmail.com>
2024-03-04 12:36:36 +0100

    change credentials.js

--- cypress/fixtures/data/credentials.js
+++ cypress/fixtures/data/credentials.js
@@ -1,5 +1,4 @@
 export default {
-
  accessToken: '11ea38574c9c0d7f67380dd845706555e0080a8bec73aa451c2a0474c4e8e8f0',
  invalidToken: '11ea38574c9c0d7f67380',
  email: Math.random().toString(5).substring(2)+"@gmail.com",
